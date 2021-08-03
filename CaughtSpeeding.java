3c255a653b5bead1516bddbae1977a8b6d799831
zhr-ebrahimzade <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-03 21:44:10 +0430

    Add files via upload

---
+++ CaughtSpeeding.java
@@ -0,0 +1,12 @@
+public class CaughtSpeeding {
+    public int caughtSpeeding(int speed, boolean isBirthday){
+        if (isBirthday)
+            speed-=5;
+        if (speed<=60)
+            return 0;
+        else if (speed<=80)
+            return 1;
+        else
+            return 2;
+    }
+}
