7ef946be5891f17f4350e2d80c78bb5673102aa7
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 18:45:14 +0430

    Delete Java/Logic-1 directory

--- Java/Logic-1/LastDigit.java
+++
@@ -1,9 +0,0 @@
-/*Given three ints, a b c, return true if two or more of them have the same rightmost digit.
-The ints are non-negative.
-Note: the % "mod" operator computes the remainder, e.g.
- 17 % 10 is 7.*/
-public class LastDigit {
-    public boolean lastDigit(int a, int b , int c ){
-        return (a%10==b%10|| b%10==c%10 || c%10==a%10);
-    }
-}
