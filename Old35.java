962a6a41649d500b7418a8d9897fc58e2afd2653
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:19:05 +0430

    Delete Old35.java

--- Old35.java
+++
@@ -1,9 +0,0 @@
-public class Old35 {
-    public boolean old35(int n){
-        if (n%15!=0){
-            if (n%3==0 || n%5==0)
-                return true;
-        }
-        return false;
-    }
-}
