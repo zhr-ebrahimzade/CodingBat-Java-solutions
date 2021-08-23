e7f822222311106b581d520e9577d73d21e3cb80
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:19:49 +0430

    Delete SquirrelPlay.java

--- SquirrelPlay.java
+++
@@ -1,8 +0,0 @@
-public class SquirrelPlay {
-    public boolean squirrelPlay(int temp,boolean isSummer){
-        if (!(isSummer))
-             return (temp<=90 && temp>=60);
-        else
-            return (temp<=100 && temp>=60);
-    }
-}
