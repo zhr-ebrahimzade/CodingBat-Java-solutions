6dfddc0dff242f7664dc3a640ec2c511a1cd2558
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:17:59 +0430

    Delete In1To10.java

--- In1To10.java
+++
@@ -1,7 +0,0 @@
-public class In1To10 {
-    public boolean in1To10(int n , boolean outsideMode) {
-        if (outsideMode)
-            return (n <= 1 || n >= 10);
-        return (n <= 10 && n >= 1);
-    }
-}
