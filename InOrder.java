37d8da260218a9040574149afec2d67ece423411
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:18:07 +0430

    Delete InOrder.java

--- InOrder.java
+++
@@ -1,9 +0,0 @@
-public class InOrder {
-    public boolean inOrder(int a, int b, int c , boolean bOk){
-        if (bOk)
-            return c>b;
-        else
-            return (c>b && b>a);
-
-    }
-}
