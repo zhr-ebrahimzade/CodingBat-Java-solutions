f5129b361586e49b239844a64850406d83962939
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:19:27 +0430

    Delete SortASum.java

--- SortASum.java
+++
@@ -1,9 +0,0 @@
-public class SortASum {
-    public int sortASum(int a , int b){
-        int sum=a + b;
-        if (sum>=10 && sum<=19)
-            return 20;
-        else
-            return sum;
-    }
-}
