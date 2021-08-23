339374e44eb97b43e82e65baf92f256b141506a7
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-08-23 16:18:14 +0430

    Delete InOrderEqual.java

--- InOrderEqual.java
+++
@@ -1,7 +0,0 @@
-public class InOrderEqual {
-    public boolean inOrderEqual(int a, int b, int c, boolean equalOk){
-        if (equalOk)
-            return  a<=b && b<=c;
-        return (b>a&& c>b);
-    }
-}
