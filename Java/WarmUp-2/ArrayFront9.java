1790a86027534d8817af78b082c246355613ba85
Zahra <88291090+zhr-ebrahimzade@users.noreply.github.com>
2021-09-02 02:01:37 +0430

    Add files via upload

---
+++ Java/WarmUp-2/ArrayFront9.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/*
+*Given an array of ints,
+* return true if one of the first 4 elements in the array is a 9.
+*  The array length may be less than 4.*/
+public class ArrayFront9 {
+    public boolean arrayFront9(int [] nums){
+        int end=nums.length;
+        if (end>4)
+            end=4;
+        for (int i=0; i<end;i++){
+            if (nums[i]==9)
+                return true;
+        }
+        return false;
+    }
+}
